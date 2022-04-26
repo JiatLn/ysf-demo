84ca4413c317008e5beefe817b20dbbb228402d5
林佳涛 <lne128gene980@163.com>
2022-04-26 15:34:17 +0800

    feat: add tabbar route

--- src/components/App/TheTabbar.vue
+++ src/components/App/TheTabbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <van-tabbar v-model="active">
-    <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
-    <van-tabbar-item icon="shop-o">商城</van-tabbar-item>
-    <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
-    <van-tabbar-item icon="user-o">个人</van-tabbar-item>
+  <van-tabbar v-model="active" route>
+    <van-tabbar-item icon="wap-home-o" :to="{ name: 'Home' }">首页</van-tabbar-item>
+    <van-tabbar-item icon="shop-o" :to="{ name: 'Store' }">商城</van-tabbar-item>
+    <van-tabbar-item icon="shopping-cart-o" :to="{ name: 'ShoppingCart' }">购物车</van-tabbar-item>
+    <van-tabbar-item icon="user-o" :to="{ name: 'UserCenter' }">个人</van-tabbar-item>
   </van-tabbar>
 </template>
 
--- src/router/modules/common.ts
+++ src/router/modules/common.ts
@@ -9,6 +9,30 @@ const routes: RouteRecordRaw[] = [
       title: '首页',
     },
   },
+  {
+    path: '/store',
+    name: 'Store',
+    component: () => import('@/pages/Store/index.vue'),
+    meta: {
+      title: '商城',
+    },
+  },
+  {
+    path: '/shopping-cart',
+    name: 'ShoppingCart',
+    component: () => import('@/pages/ShoppingCart/index.vue'),
+    meta: {
+      title: '购物车',
+    },
+  },
+  {
+    path: '/user-center',
+    name: 'UserCenter',
+    component: () => import('@/pages/UserCenter/index.vue'),
+    meta: {
+      title: '个人中心',
+    },
+  },
 ];
 
 export default routes;
